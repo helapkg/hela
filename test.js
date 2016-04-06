5de8edf78d1e41ac9813955d6075870a333d9c71
tunnckoCore <mameto_100@mail.bg>
2016-04-06 23:27:21 +0300

    update urlencoded tests

--- test.js
+++ test.js
@@ -48,20 +48,31 @@ test('parse json body', function () {
   })
 })
 
-test('should parse a urlencoded body', function (done) {
-  var server = koa().use(betterBody())
-  server.use(function * () {
-    test.strictEqual(typeof this.request.fields, 'object')
-    test.strictEqual(typeof this.body, 'object')
-    test.deepEqual(this.body, {a: 'b', c: 'd'})
-    test.deepEqual(this.request.fields, {a: 'b', c: 'd'})
+test('parse urlencoded (forms) body', function () {
+  test('should parse a urlencoded body', function (done) {
+    var server = koa().use(betterBody())
+    server.use(function * () {
+      test.strictEqual(typeof this.request.fields, 'object')
+      test.strictEqual(typeof this.body, 'object')
+      test.deepEqual(this.body, {a: 'b', c: 'd'})
+      test.deepEqual(this.request.fields, {a: 'b', c: 'd'})
+    })
+    request(server.callback())
+      .post('/')
+      .type('application/x-www-form-urlencoded')
+      .send('a=b&c=d')
+      .expect(200)
+      .expect(/"a":"b"/)
+      .expect(/"c":"d"/, done)
   })
-  request(server.callback())
-    .post('/')
-    .send('a=b&c=d')
-    .expect(200)
-    .expect(/"a":"b"/)
-    .expect(/"c":"d"/, done)
+  test('should throw if the body is too large', function (done) {
+    var server = koa().use(betterBody({formLimit: '2b'}))
+    request(server.callback())
+      .post('/')
+      .type('application/x-www-form-urlencoded')
+      .send('a=b&c=d')
+      .expect(413, done)
+  })
 })
 
 test('parse text body', function () {
