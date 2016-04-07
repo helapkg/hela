a50346651981233df70728661d14e8c31829bc2d
tunnckoCore <mameto_100@mail.bg>
2016-04-07 05:06:52 +0300

    simplify, DRY, SoC

--- index.js
+++ index.js
@@ -17,32 +17,12 @@ module.exports = function koaBetterBody (options) {
       return yield * next
     }
 
-    utils.parseBody(this)
-
-    var fields = typeof options.fields === 'string' ? options.fields : 'fields'
-    var files = typeof options.files === 'string' ? options.files : 'files'
-
-    if (options.detectJSON(this) || this.request.is(options.extendTypes.json)) {
-      this.app.jsonStrict = typeof options.jsonStrict === 'boolean' ? options.jsonStrict : true
-      this.body = this.request[fields] = yield this.request.json(options.jsonLimit)
-      return yield * next
-    }
-    if (this.request.is(options.extendTypes.form || options.extendTypes.urlencoded)) {
-      this.body = this.request[fields] = yield this.request.urlencoded(options.formLimit)
-      return yield * next
-    }
-    if (this.request.is(options.extendTypes.text)) {
-      this.body = options.buffer
-        ? yield this.request.buffer(options.bufferLimit || options.textLimit)
-        : yield this.request.text(options.textLimit)
-      return yield * next
-    }
-    if (this.request.is(options.extendTypes.multipart)) {
-      var result = yield this.request.multipart(options, this)
-      this.request[fields] = result.fields
-      this.request[files] = result.files
-      this.body = !options.fields ? this.request[fields] : undefined
-      return yield * next
+    try {
+      utils.setParsers(this)
+      yield * utils.parseBody(this, options, next)
+    } catch (err) {
+      if (!options.onerror) throw err
+      options.onerror(err, this)
     }
 
     yield * next
--- utils.js
+++ utils.js
@@ -26,38 +26,13 @@ require = utils // eslint-disable-line no-undef, no-native-reassign
 
 require('extend-shallow', 'extend')
 require('formidable')
-require('koa-body-parsers', 'parse')
+require('koa-body-parsers', 'bodyParsers')
 
 /**
  * Restore `require`
  */
 
 require = fn // eslint-disable-line no-undef, no-native-reassign
-
-utils.parseBody = function parseBody (ctx) {
-  utils.parse(ctx)
-  ctx.request.multipart = utils.multipart
-  return ctx
-}
-
-utils.multipart = function multipart (options, ctx) {
-  if (typeof ctx !== 'object') {
-    ctx = options
-    options = {}
-  }
-  options = utils.defaultOptions(options)
-
-  return function thunk (done) {
-    var form = options.IncomingForm instanceof utils.formidable.IncomingForm
-      ? options.IncomingForm
-      : new utils.formidable.IncomingForm(options)
-
-    form.parse(ctx.req, function callback (err, fields, files) {
-      if (err) return done(err)
-      done(null, { fields: fields, files: files })
-    })
-  }
-}
 
 utils.defaultOptions = function defaultOptions (options) {
   options = typeof options === 'object' ? options : {}
@@ -75,6 +50,8 @@ utils.defaultOptions = function defaultOptions (options) {
     strict: true
   }, options)
   options.extendTypes = types
+  options.onerror = options.onЕrror || options.onerror
+  options.onerror = typeof options.onerror === 'function' ? options.onerror : false
 
   if (typeof options.detectJSON !== 'function') {
     options.detectJSON = function detectJSON () {
@@ -108,6 +85,59 @@ utils.defaultTypes = function defaultTypes (types) {
 
 utils.isValid = function isValid (method) {
   return ['GET', 'HEAD', 'DELETE'].indexOf(method.toUpperCase()) === -1
+}
+
+utils.setParsers = function setParsers (ctx) {
+  utils.bodyParsers(ctx)
+  ctx.request.multipart = utils.multipart
+}
+
+utils.multipart = function multipart (options, ctx) {
+  if (typeof ctx !== 'object') {
+    ctx = options
+    options = {}
+  }
+  options = utils.defaultOptions(options)
+
+  return function thunk (done) {
+    var form = options.IncomingForm instanceof utils.formidable.IncomingForm
+      ? options.IncomingForm
+      : new utils.formidable.IncomingForm(options)
+
+    form.parse(ctx.req, function callback (err, fields, files) {
+      if (err) return done(err)
+      done(null, { fields: fields, files: files })
+    })
+  }
+}
+
+/* eslint complexity: [2, 12] */
+utils.parseBody = function * parseBody (ctx, options, next) {
+  var fields = typeof options.fields === 'string' ? options.fields : 'fields'
+  var files = typeof options.files === 'string' ? options.files : 'files'
+
+  if (options.detectJSON(ctx) || ctx.request.is(options.extendTypes.json)) {
+    ctx.app.jsonStrict = typeof options.jsonStrict === 'boolean' ? options.jsonStrict : true
+    ctx.body = ctx.request[fields] = yield ctx.request.json(options.jsonLimit)
+    return yield * next
+  }
+  if (ctx.request.is(options.extendTypes.form || options.extendTypes.urlencoded)) {
+    var res = yield ctx.request.urlencoded(options.formLimit)
+    ctx.body = ctx.request[fields] = res
+    return yield * next
+  }
+  if (ctx.request.is(options.extendTypes.text)) {
+    ctx.body = options.buffer
+      ? yield ctx.request.buffer(options.bufferLimit || options.textLimit)
+      : yield ctx.request.text(options.textLimit)
+    return yield * next
+  }
+  if (ctx.request.is(options.extendTypes.multipart)) {
+    var result = yield ctx.request.multipart(options, ctx)
+    ctx.body = ctx.request[fields] = result.fields
+    ctx.request[files] = result.files
+    return yield * next
+  }
 }
 
 /**
