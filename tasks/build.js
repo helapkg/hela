95f42e9fe95e3d4f399f5c09d37c71967059e12c
tunnckoCore <mameto2011@gmail.com>
2017-08-11 17:55:01 +0300

    feat(tasks): add build, pretest and posttest tasks
    TAG: latest
    
    closes #3

---
+++ tasks/build.js
@@ -0,0 +1,11 @@
+/*!
+ * hela <https://github.com/tunnckoCore/hela>
+ *
+ * Copyright (c) 2017 Charlike Mike Reagent <open.source.charlike@gmail.com> (https://i.am.charlike.online)
+ * Released under the MIT license.
+ */
+
+module.exports = ({ shell }) => {
+  console.log('Running `build` command...')
+  shell('rollup -c')
+}
---
+++ tasks/posttest.js
@@ -0,0 +1,12 @@
+/*!
+ * hela <https://github.com/tunnckoCore/hela>
+ *
+ * Copyright (c) 2017 Charlike Mike Reagent <open.source.charlike@gmail.com> (https://i.am.charlike.online)
+ * Released under the MIT license.
+ */
+
+const { shell } = require('execa')
+
+module.exports = ({ app }) => {
+  shell("sed -i 's/dest/src/' test.js").catch((er) => app.emit('error', er))
+}
