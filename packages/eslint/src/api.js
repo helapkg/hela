9b8053cb3a170fb0cfce15dd83cc99e8e969031c
Charlike Mike Reagent <opensource@tunnckocore.com>
2020-03-08 04:25:04 +0200

    cleanup & renames
    Signed-off-by: Charlike Mike Reagent <opensource@tunnckocore.com>

--- packages/eslint/src/api.js
+++ packages/eslint/src/api.js
@@ -13,7 +13,7 @@ const memoizeFs = require('memoize-fs');
 const globCache = require('glob-cache');
 const utils = require('./utils');
 
-async function* resolveFilesStream(patterns, options) {
+async function* resolvePatternsStream(patterns, options) {
   const opts = { ...utils.constants.DEFAULT_OPTIONS, ...options };
   const iterable = await globCache(patterns, opts);
 
@@ -146,19 +146,6 @@ async function resolvePatterns(patterns, options) {
   return results;
 }
 
-/**
- * Resolve and lint given glob patterns.
- *
- * @param {string|string[]} patterns - glob patterns to match
- * @param {*} options
- */
-// async function processFiles(patterns, options) {
-//   const opts = { ...utils.constants.DEFAULT_OPTIONS, ...options };
-//   const files = await resolveFiles(patterns, opts);
-
-//   return lintFiles(files, opts);
-// }
-
 async function lintText(contents, options) {
   const opts = { ...utils.constants.DEFAULT_OPTIONS, ...options };
   const source = contents;
@@ -171,77 +158,6 @@ async function lintText(contents, options) {
     fixableWarningCount: 0,
   };
 }
-
-/**
- * Lint already resolved Files, Contexts or full filepaths, or an array of them
- *
- * @param {File|File[]|Context|Context[]|string|string[]} items
- * @param {*} options
- */
-// async function lintItems(items, options) {
-//   const opts = { ...utils.constants.DEFAULT_OPTIONS, ...options };
-//   const report = {
-//     results: [],
-//     errorCount: 0,
-//     warningCount: 0,
-//   };
-
-//   await Promise.all(
-//     []
-//       .concat(items)
-//       .filter(Boolean)
-//       .map(async (item) => {
-//         if (opts.type === 'paths' || opts.type === 'files') {
-//           const file = await utils.toFile(await item);
-
-//           const repFromText = await lintText(file.contents, opts);
-
-//           // calcReport(report, repFromText);
-//           report.results.push(file.path);
-
-//           await cacache.put(opts.cacheLocation, file.path, repFromText.source, {
-//             metadata: {
-//               report: repFromText,
-//               config: opts.config,
-//             },
-//           });
-
-//           return;
-//         }
-//         if (opts.type === 'contexts') {
-//           const { file, cacheFile } = await item;
-//           const meta = cacheFile && cacheFile.metadata;
-//           const { source, ...repFromText } = await lintText(
-//             file.contents,
-//             opts,
-//           );
-
-//           const reportChanged =
-//             JSON.stringify(repFromText) !== JSON.stringify(meta && meta.report);
-
-//           if (opts.forceLoad === true || reportChanged) {
-//             // calcReport(report, repFromText);
-//             report.results.push(file.path);
-
-//             await cacache.put(opts.cacheLocation, file.path, source, {
-//               metadata: {
-//                 report: { ...repFromText, source },
-//                 config: opts.config,
-//               },
-//             });
-//           }
-//         }
-//       }),
-//   );
-
-//   return report;
-// }
-
-// async function lintFiles(files, options) {
-//   return lintItems(files, { ...options, type: 'files' });
-// }
-
-// resolveFiles('modules/*/src/**/*.js', { forceLoad: true }).then(console.log);
 
 lintFiles([
   // 'foobar.js',
